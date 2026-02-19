43b636bbbc8249e2576b1f8b5d88b57dd3758eb4
aldiumri <aldiumriwiryachandra0410@gmail.com>
2026-02-19 14:38:43 +0700

    Update pemanggil.js
    30%

--- pemanggil.js
+++ pemanggil.js
@@ -1,33 +1,7 @@
-
-
-
-/*let users = [];
-
-const masuk = confirm("Selamat Datang Di Portofolio Saya");
-if (masuk){
-    const name = prompt("Siapa Nama Anda?");
-    alert(`Hello ${name}`);
-} else {
-    alert("Bye Bye");
-} */
-
- /*  const aboutMe = {
-      name: "Aldi Chandra",
-      job: "Web Developer",
-      hobby: "Coding & Design",
-      bio: "Saya suka membuat website menggunakan HTML, CSS, dan JavaScript."
-    };
-  
-    document.getElementById("about").innerHTML = `
-      <p><b>Nama:</b> ${aboutMe.name}</p>
-      <p><b>Pekerjaan:</b> ${aboutMe.job}</p>
-      <p><b>Hobi:</b> ${aboutMe.hobby}</p>
-      <p>${aboutMe.bio}</p>
-    `; */
-
-    
 function showabout() {
  window.location.href = "about.html"
 };
- 
-  
+
+function showporto(){
+  window.location.href= "halaman.html"
+}
